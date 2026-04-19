f633f2f4ae17cfc2b342ae4f9f5ace1d4c5eb80c
Mybell-TN <ngocnguyen030312@gmail.com>
2026-04-19 17:46:06 +0700

    Update loopFor

--- Day3/syntax_caudieukien.js
+++ Day3/syntax_caudieukien.js
@@ -1,1 +1,38 @@
-console.log("Liên kết thành công")
+var age = 30;
+
+if (age >= 30) {
+    console.log("Đủ tuổi uống rượu");
+} else {
+    console.log("Không đủ tuổi uống rượu");
+}
+
+// ===================================
+var toan = 1;
+var ly = 5;
+var tiengAnh = 10;
+var diemTB = (toan + ly + tiengAnh) / 3;
+
+if (diemTB >= 9 && diemTB <= 10) 
+{
+    console.log("Xuất sắc");
+} 
+else if (diemTB >= 8 && diemTB <= 8.9)
+{
+    console.log("Điểm giỏi");
+}
+else if (diemTB >= 7 && diemTB <= 7.9)
+{
+    console.log("Điểm Khá");
+}
+else if (diemTB >= 5 && diemTB <= 6.9)
+{
+    console.log("Điểm Trung Bình");
+} 
+else if (diemTB >= 4 && diemTB <= 4.9)
+{
+    console.log("Điểm Yếu");
+}
+else
+{
+    console.log("Điểm Kém");
+}
